8b0c093f0544cc7dae305f936ec45cf4457a37e5
SleepyJeno <kalinskij.zhenya@gmail.com>
2024-01-06 12:47:21 +0000

    removed unused imports; refactored Cipher into struct

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,60 @@
 use rand::RngCore;
-use rand::{Rng, distributions::Alphanumeric};
 use aes::Aes128;
 use block_modes::{BlockMode, Cbc};
 use block_modes::block_padding::Pkcs7;
-use hex_literal::hex;
 type Aes128Cbc = Cbc<Aes128, Pkcs7>;
+
+
+struct Cipher {
+    key: Vec<u8>,
+    iv: Vec<u8>,
+    cipher: Aes128Cbc
+}
+
+impl Cipher {
+    fn new() -> Self{
+        let key = Self::generate_key();
+        let iv = Self::generate_iv();
+        //Aes128Cbc::new_from_slices(&key, &iv).unwrap()
+        let cipher: Cbc<Aes128, Pkcs7> = Aes128Cbc::new_from_slices(&key, &iv).unwrap();
+
+        Self {
+            key,
+            iv,
+            cipher
+          }
+    }
+
+    fn generate_key() -> Vec<u8> {
+        let mut bytes = [0; 16];
+        rand::thread_rng().fill_bytes(&mut bytes);
+        bytes.to_vec()
+    }
+
+    fn generate_iv() -> Vec<u8> {
+        let mut bytes = [0; 16];
+        rand::thread_rng().fill_bytes(&mut bytes);
+        bytes.to_vec()
+    }
+}
 
 fn main() {
     println!("Hello, world!");
-    tokenize("blah");
-}
-
-fn generate_key() -> Vec<u8> {
-    let mut bytes = [0; 16];
-    rand::thread_rng().fill_bytes(&mut bytes);
-
-    bytes.to_vec()
-}
-
-fn generate_iv() -> Vec<u8> {
-    let mut bytes = [0; 16];
-    rand::thread_rng().fill_bytes(&mut bytes);
-
-    bytes.to_vec()
+    println!("{:?}", tokenize("blah"));
 }
 
 fn tokenize(input: &str) -> String {
-    let key = generate_key();
-    let iv = generate_iv();
+    let cipher_base = Cipher::new();
     let plaintext = input.as_bytes();
-    let cipher = Aes128Cbc::new_from_slices(&key, &iv).unwrap();
-
     let pos = plaintext.len();
     let mut buffer = [0u8; 128];
     buffer[..pos].copy_from_slice(plaintext);
-    let ciphertext = cipher.encrypt(&mut buffer, pos).unwrap();
+    let ciphertext = cipher_base.cipher.encrypt(&mut buffer, pos).unwrap();
 
     hex::encode(ciphertext)
+}
+
+fn detokenize(input: &str, cipher: Cbc<Aes128, Pkcs7>) -> String {
+
+    String::from("a")
 }
