07e1e3669645abb2003b8ddce92908df34d765b9
SleepyJeno <kalinskij.zhenya@gmail.com>
2024-01-07 14:21:06 +0000

    added docker-compose to run both containers in a swarm

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,4 @@ WORKDIR ./rust_tokenizer
 
 RUN cargo build --release
 
-ENV DB_USERNAME=postgres
-ENV DB_PASSWORD=password 
-ENV DB_URL='127.0.0.1' 
-ENV DB_NAME=tokenizer_db
-ENV DB_PORT=5443
-
 ENTRYPOINT [ "./target/release/rust_tokenizer" ]
--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
     let credentials = Credentials {
         username: std::env::var("DB_USERNAME").expect("DB_USERNAME must be set"),
         password: std::env::var("DB_PASSWORD").expect("DB_PASSWORD must be set"),
-        host: std::env::var("DB_URL").expect("DB_URL must be set"),
+        host: std::env::var("DB_HOST").expect("DB_HOST must be set"),
         port: std::env::var("DB_PORT").expect("DB_PORT must be set"),
         db_name: std::env::var("DB_NAME").expect("DB_NAME must be set"),
         ssl_mode: std::env::var("DB_SSL_MODE").unwrap_or(String::from("disable"))
