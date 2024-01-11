1243b7b1395b954305546726bcecb47aa4225eeb
SleepyJeno <kalinskij.zhenya@gmail.com>
2024-01-11 15:43:53 +0000

    split tokenisation logic into a module

---
+++ src/tokenization.rs
@@ -0,0 +1,23 @@
+use postgres::Client;
+use crate::cipher::Cipher;
+use crate::db;
+
+
+pub fn tokenize(input: &str, cipher: &Cipher, client: &mut Client) -> String {
+    //TODO: add check for input string size to be less than 128 bytes
+    let plaintext = input.as_bytes();
+    let ciphertext = cipher.encrypt(&plaintext);
+    let tokenized_string = hex::encode(ciphertext);
+    let (key, iv) = cipher.get_cipher_data();
+    db::queries::write_tokenized_data(client, &tokenized_string, &key, &iv).ok();
+    tokenized_string
+}
+
+pub fn detokenize(tokenized_string: &String, client: &mut Client) -> String {
+    let (tokenised_string, key, iv) = db::queries::read_tokenized_data(client, tokenized_string).unwrap();
+    let cipher = Cipher::from(key, iv);
+    let ciphertext = hex::decode(tokenised_string).unwrap();
+    let buffer = ciphertext.to_vec();
+    let decrypted = cipher.decrypt(&buffer);
+    String::from_utf8(decrypted.to_vec()).unwrap()
+}
