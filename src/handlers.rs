06cb5028366a639c6e1be5748c15054da89f485a
SleepyJeno <kalinskij.zhenya@gmail.com>
2024-01-14 11:45:19 +0000

    added tokenize endpoint; start refactoring

---
+++ src/handlers.rs
@@ -0,0 +1,35 @@
+use actix_web::{self, HttpResponse, Responder, web};
+use crate::tokenization;
+use crate::db;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TokenizeRequest {
+    pub input: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TokenizeResponse {
+    pub result: String,
+}
+
+// Implement Default for TokenizeResponse
+// This allows Actix to automatically generate responses with a default value
+impl Default for TokenizeResponse {
+    fn default() -> Self {
+        TokenizeResponse {
+            result: String::new(),
+        }
+    }
+}
+
+#[actix_web::post("/tokenize")]
+async fn tokenize(data: web::Json<TokenizeRequest>) -> impl Responder  {
+    let input = &data.input;
+
+    let result = tokenization::tokenize_str(input);
+    let response: TokenizeResponse = TokenizeResponse { result };
+    HttpResponse::Ok().json(response)
+}
+
+
--- src/tokenization.rs
+++ src/tokenization.rs
@@ -2,18 +2,18 @@ use postgres::Client;
 use crate::cipher::Cipher;
 use crate::db;
 
-
-pub fn tokenize(input: &str, cipher: &Cipher, client: &mut Client) -> String {
+pub fn tokenize_str(input: &str) -> String {
     //TODO: add check for input string size to be less than 128 bytes
+    let cipher = Cipher::new();
     let plaintext = input.as_bytes();
     let ciphertext = cipher.encrypt(&plaintext);
     let tokenized_string = hex::encode(ciphertext);
     let (key, iv) = cipher.get_cipher_data();
-    db::queries::write_tokenized_data(client, &tokenized_string, &key, &iv).ok();
+    //db::queries::write_tokenized_data(client, &tokenized_string, &key, &iv).ok();
     tokenized_string
 }
 
-pub fn detokenize(tokenized_string: &String, client: &mut Client) -> String {
+pub fn detokenize_str(tokenized_string: &String, client: &mut Client) -> String {
     let (tokenised_string, key, iv) = db::queries::read_tokenized_data(client, tokenized_string).unwrap();
     let cipher = Cipher::from(key, iv);
     let ciphertext = hex::decode(tokenised_string).unwrap();
